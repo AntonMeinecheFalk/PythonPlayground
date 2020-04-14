cfae4a0c75b7d8c1995304fc19f7a59ad79c622a
Anton Meineche Falk <anton.m.falk.illustration@gmail.com>
2020-04-14 23:24:52 +0200

    Hello world and variables

---
+++ Variables.py
@@ -0,0 +1,17 @@
+# nameofvar = value
+
+# Data types
+
+name                          = "Buttman"  # string, / text                ("this is what a string is, dummy")
+age                           = 34         # integer / whole number        (4)
+numberOfPoopsTakenThisMorning = 2.5        # float   / fractional number   (3.4542)
+isSingleAndReadyToMingle      = True       # boolean / true/false          (true)
+Professor                     = 'x'        # char    / letter              ('x')    
+
+
+
+print("Hello " + name + " you smell like shit")
+print("i bet a sad " + str(age) + " Year old man like you,")
+print("takes at least " + str(numberOfPoopsTakenThisMorning) + " shits every morning")
+print("I mean " + str(isSingleAndReadyToMingle) + " the sad middle aged man said,")
+print("And put an " + str(Professor) + " in his callender for planned murder on imaginary girlfriend")
